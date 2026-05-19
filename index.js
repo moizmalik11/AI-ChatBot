78987e608b99ee8762ec835df0ccf0f24abc6dac
Moiz Malik <moiz.malik1113@gmail.com>
2026-05-19 23:55:43 +0500

    add bot icon to message display in appendMessage function

--- index.js
+++ index.js
@@ -133,4 +133,12 @@ function appendMessage(sender, message) {
         iconElement.setAttribute('id', 'user-icon');
     } else {
         icon.classList.add('fa-solid', 'fa-robot');
-        
+        iconElement.setAttribute('id', 'bot-icon');
+    }
+
+    iconElement.appendChild(icon);
+    chatElement.appendChild(iconElement);
+    chatElement.appendChild(messageElement);
+    chatLog.appendChild(chatElement);
+
+}
